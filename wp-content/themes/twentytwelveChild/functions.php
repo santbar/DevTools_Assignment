1b562c66e8701d0368fbd83b1228111d8f52088f
Santiago Barrera <santisb2@gmail.com>
2018-02-26 17:33:58 -0500

    Add google fonts

--- wp-content/themes/twentytwelveChild/functions.php
+++ wp-content/themes/twentytwelveChild/functions.php
@@ -1,6 +1,8 @@
 <?php
 
-function wpd_add_google_fonts(){
-  wp_enqueue_style('wpb-google-fonts','https://fonts.googleaplis.com/css?family=Joti+One|ranga',false);
-}
-add_action('wp_enqueue_scripts','wpb_add_google_fonts');
+function custom_add_google_fonts() {
+ wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,700,900|Roboto:300,400,700', false );
+ }
+ add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
+
+ ?>
