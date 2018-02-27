e947abd7e4a7c1b312a645ecc700f191dfd887d3
Santiago Barrera <santisb2@gmail.com>
2018-02-26 19:08:07 -0500

    First changes to templates

---
+++ wp-content/themes/twentytwelveChild/single-post-size.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: single_noComments.php
+ * The template that remove comments out of pages.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<nav class="nav-single">
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+				</nav><!-- .nav-single -->
+
+				<?php //comments_template( '', true ); ?><!--Here I remove the comments out of the page-->
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php //get_sidebar(); ?><!--Here I removed the sidebar-->
+<?php get_footer(); ?>
